cc32aea20d6191ebaf7b362ff4e778a8f4ccdcb7
gewinfx <ramazankantaev95@gmail.com>
2026-04-13 12:37:09 +0600

    ss

--- payment-service/internal/transport/http/grcp/server.go
+++ payment-service/internal/transport/http/grcp/server.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"payment-service/internal/usecase"
-	"project/proto/paymentpb"
+	"payment-service/proto/paymentpb"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
